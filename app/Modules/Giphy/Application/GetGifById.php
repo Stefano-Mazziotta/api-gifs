8fde56a776e88ecb3ce3274dc80323f37dee378b
stefano mazziotta <smazziotta@foodlus.com>
2024-11-18 21:41:41 -0300

    dev - implement search gifs and search gif by id

--- app/Modules/Giphy/Application/GetGifById.php
+++ app/Modules/Giphy/Application/GetGifById.php
@@ -2,25 +2,19 @@
 
 namespace App\Modules\Giphy\Application;
 
-use GuzzleHttp\Client;
+use App\Modules\Giphy\Domain\Repositories\GifRepositoryInterface;
 
 class GetGifById
 {
-    protected $client;
+    private $gifRepository;
 
-    public function __construct()
+    public function __construct(GifRepositoryInterface $gifRepository)
     {
-        $this->client = new Client(['base_uri' => 'https://api.giphy.com/v1/']);
+        $this->gifRepository = $gifRepository;
     }
 
-    public function execute($id)
+    public function execute(string $id)
     {
-        $response = $this->client->get("gifs/$id", [
-            'query' => [
-                'api_key' => env('GIPHY_API_KEY'),
-            ]
-        ]);
-
-        return json_decode($response->getBody()->getContents(), true);
+        return $this->gifRepository->getGifById($id);
     }
 }
